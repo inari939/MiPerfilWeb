1d0a81f949449bb4814286e0fcd2d1ca5dea5b26
Alexandra Ramirez <alexandraramirez939@gmail.com>
2023-07-07 18:56:19 -0300

    hice un breve formulario para que el usuario tambien pueda usarlo como metodo de contacto

---
+++ scripts.js
@@ -0,0 +1,11 @@
+
+//mostrar articulos//
+document.getElementById('boton-mostrar').addEventListener('click', function(){
+    document.getElementById('formulario').style.display = 'flex'
+});
+
+
+//ocultar articulos//
+document.getElementById('boton-ocultar').addEventListener('click', function(){
+    document.getElementById('formulario').style.display = 'none'
+});
